1ea97d9152620105a82ee79b4fd7f5eb274cb98e
joshi304 <55596316+joshi304@users.noreply.github.com>
2021-11-04 20:30:57 -0500

    fixed deploy script

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,2 @@
 @echo off
-npm run build&&gh-pages -d dist
+npm run build&&cd dist&&git init&&git add -A&&git commit -m 'deploy'&&git push -f git@github.com:IshanPJoshi/command-deck-builder.git main:gh-pages&&cd -
