27021d5bd706df20a26578f2310c911f15c397bf
joshi304 <joshi304@umn.edu>
2021-12-26 00:15:56 -0600

    element-plus basics

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,40 @@
 <template>
   <div>
-    <H3>Hello {{name}}</H3>
+    <h2>Hello {{name}}</h2>
   </div>
+  <el-dropdown v-for="(c, key) in category" v-bind:key="c" style="padding:20px;">
+    <el-button type="primary">
+      {{key}}
+    </el-button>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item v-for="item in c" v-bind:key="item">{{item}}</el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+
+  <!-- <div class="block">
+    <span class="demonstration">Default</span>
+    <el-date-picker
+      v-model="value3"
+      type="datetimerange"
+      range-separator="To"
+      start-placeholder="Start date"
+      end-placeholder="End date"
+    >
+    </el-date-picker>
+  </div>
+  {{value3}} -->
+
+    
 </template>
 
 <script setup>
+// import {ref} from 'vue'
 let name = "Ishan"
+// const value3 = ref(0)
+let category = {A: ["BigA", "SmallA"],
+            B: ["BigB", "SmallB"]}
 </script>
-
 <style lang="scss" scoped>
-
 </style>
